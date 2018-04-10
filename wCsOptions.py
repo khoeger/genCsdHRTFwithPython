6e75ebfe50a5ad12b8e5dd3c6def5b3c7257987c
khoeger <katarina.hoeger@gmail.com>
2018-04-10 17:56:32 -0400

    Add suppression of output and better amplitude control

--- wCsOptions.py
+++ wCsOptions.py
@@ -3,7 +3,9 @@ def writeCsOptions(f, realtime=True,savePath = ""):
     f.write('; Select flags here\n')
     if realtime == True:
         f.write('; realtime audio out\n')
-        f.write('-o dac \n')
+        f.write('-o dac \n')# -o dac: Write sound to the host audio output device
+        f.write('-m35\n')   # -m35 =? -m 35 = -m 1+2+32 = note applitude message, samples out of range message, & no colors
+        f.write('-d\n')     # -d: surpresses displays
     if savePath != "":
         f.write('; For Non-realtime ouput leave only the line below:\n')
         f.write(' -o '+savePath+'\n')
