b2975ae902ac6fed3759293eee1def51a1438e48
khoeger <katarina.hoeger@gmail.com>
2018-04-21 17:41:58 -0400

    Test csound score reading into python

--- main.py
+++ main.py
@@ -5,15 +5,27 @@
 """
 #  Import packages/libraries
 import ctcsound
+import numpy as np
+import pandas as pd
+
+import csScoreToPython
 import wCsOptions
 import wCsInstruments
 import wCsScore
 
+
+
 # Define variables
+readFileName = "cabs_CsScore_v1.cs"
 outCsndFile = "hrtfmove2.csd"
 sr = 96000#44100  # audio sample rate,
 ksmps = 1   # control rate
 nchnls =2   # number of channels
+
+stftOverlaps = 4
+headRadius = 8.15 #cm
+
+fTableSize = 2**20
 
 # Create class object instances
 cs = ctcsound.Csound()
@@ -25,38 +37,45 @@ with open(outCsndFile,'w') as f:
     f.write('<CsoundSynthesizer>\n\n')
 
     # -- Decide on Options
-    #wCsOptions.writeCsOptions(f,savePath="hrtfSinVersion.wav")
+    #wCsOptions.writeCsOptions(f,savePath="movingSound_4_19_2018.wav")
     wCsOptions.writeCsOptions(f)
 
     # -- Start creating CsInstruments
     f.write('<CsInstruments>\n\n')
     wCsInstruments.wInstrumentForematter(f,sr,ksmps,nchnls)
-    wCsInstruments.sinInstr(f,1,'p4','p5',1,'p3') # sine instr
-    wCsInstruments.hrtfMove2ExInstr(f,1,720,0,'p3',-10,0,'p3') #hrtf instr
+    wCsInstruments.sinInstr(f, 1,'p4','p5',1,'p3') # sine instr
+    wCsInstruments.hrtfMove2ExInstr(f, 1, 720, 0, 'p3', -1000, 0, 'p3',
+                                    sr, stftOverlaps, headRadius) #hrtf instr
     f.write('</CsInstruments>\n')
 
     # -- Start creating CsScore
     f.write('<CsScore>\n\n')
     # function table
-    wCsScore.funcTable(f,1, 0, 2**20, 10, [1])
+    wCsScore.funcTable(f,1, 0, fTableSize, 10, [1])
     # verse 1 - named
     f.write('\nm v1\n\n')
+    f.write('\nt 0 120\n\n')
+    scoreLines = csScoreToPython.readScore(readFileName)
+    npScoreLines = np.asarray(scoreLines)
+    print(npScoreLines[:10])
+
+
     # instrument 1 - http://www.csounds.com/manual/html/ScoreStatements.html
-    wCsScore.playNote(f, 1, 0.0, 0.2, [15000, 440.00] )
-    wCsScore.playNote(f, 1, 0.2, 0.2, [15000, 466.16])
-    wCsScore.playNote(f, 1, 0.4, 0.2, [15000, 493.88 ])
-    wCsScore.playNote(f, 1, 0.6, 0.2, [15000, 523.25 ])
-    wCsScore.playNote(f, 1, 0.8, 0.2, [15000, 587.33])
-    wCsScore.playNote(f, 1, 1.0, 1.5, [15000, 659.25])
-    wCsScore.playNote(f, 1, 2.5 ,1.5, [15000, 698.46])
-    wCsScore.playNote(f, 1, 4.0, 1.5, [15000, 783.99])
-    wCsScore.playNote(f, 1, 5.5, 1.5, [15000, 880.00])
-    wCsScore.playNote(f, 1, 7.0, 1.5, [15000, 830.6])
-    wCsScore.playNote(f, 1, 8.5, 1.5, [15000, 783.99])
-    wCsScore.playNote(f, 1.1, 10.0, 1.5, [7500, 987.77])
-    wCsScore.playNote(f, 1.2, 10.0, 1.5, [7500, 783.99])
-    wCsScore.playNote(f, 1.1, 11.5, 4.0, [7500, 659.25])
-    wCsScore.playNote(f, 1.2, 11.5, 4.0, [7500, 783.99])
+    wCsScore.playNote(f, 1, 0.0, 0.2, [8000, 440.00] )
+    wCsScore.playNote(f, 1, 0.2, 0.2, [8000, 466.16])
+    wCsScore.playNote(f, 1, 0.4, 0.2, [8000, 493.88 ])
+    wCsScore.playNote(f, 1, 0.6, 0.2, [8000, 523.25 ])
+    wCsScore.playNote(f, 1, 0.8, 0.2, [8000, 587.33])
+    wCsScore.playNote(f, 1, 1.0, 1.5, [8000, 659.25])
+    wCsScore.playNote(f, 1, 2.5 ,1.5, [8000, 698.46])
+    wCsScore.playNote(f, 1, 4.0, 1.5, [8000, 783.99])
+    wCsScore.playNote(f, 1, 5.5, 1.5, [8000, 880.00])
+    wCsScore.playNote(f, 1, 7.0, 1.5, [8000, 830.6])
+    wCsScore.playNote(f, 1, 8.5, 1.5, [8000, 783.99])
+    wCsScore.playNote(f, 1.1, 10.0, 1.5, [4000, 987.77])
+    wCsScore.playNote(f, 1.2, 10.0, 1.5, [4000, 783.99])
+    wCsScore.playNote(f, 1.1, 11.5, 4.0, [2000, 659.25])
+    wCsScore.playNote(f, 1.2, 11.5, 4.0, [2000, 783.99])
     f.write('\n')
     # instrument 10
     wCsScore.playNote(f, 10, '0', 15.5, [])
